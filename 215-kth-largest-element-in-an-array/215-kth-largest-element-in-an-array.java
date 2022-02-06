62e4669975071a78c662351162f4482a417161e9
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-02-06 23:11:21 +0530

    Time: 43 ms (5.18%), Space: 53.8 MB (5.71%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    class Edge
+    {
+        int x;
+        public Edge(int x)
+            {this.x=x;}
+    }
+    class EdgeComparator implements Comparator<Edge>
+    {
+        @Override
+        public int compare(Edge s1, Edge s2)
+        {
+            if(s1.x>=s2.x)
+                return -1;
+            else
+                return 1;
+        }
+    }
+    public int findKthLargest(int[] arr, int k) {
+        PriorityQueue<Edge> pq = new PriorityQueue<Edge> (k, new EdgeComparator());
+        for(int i=0;i<arr.length;i++)
+        {
+            pq.offer(new Edge(arr[i]));
+        }
+        System.out.println(pq);
+        int a=-1;
+        while(!pq.isEmpty()&&k>0)
+        {
+            a = pq.poll().x;k--;
+        }
+        return a;
+    }
+}
