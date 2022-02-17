742e40fc6bd540cbeaec1d5b8103b0bbddba00b4
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-02-17 11:08:34 +0530

    Time: 26 ms (7.90%), Space: 54 MB (5.12%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] arr, int k) {
         List<List<Integer>> list = new ArrayList<>();
-        Map<ArrayList<Integer> , Integer> mp = new HashMap();
-        call(mp,arr,k,0,0,new ArrayList<Integer>());
-        for (Map.Entry<ArrayList<Integer> , Integer> entry : mp.entrySet())
-            list.add(entry.getKey());
+        
+        call(list,arr,k,0,0,new ArrayList<Integer>());
         return list;
     }
-public void call(Map<ArrayList<Integer>,Integer> mp,int[] arr,int k,int i,int curr,ArrayList<Integer> l)
+    public void call(List<List<Integer>> list,int[] arr, int k, int i, int curr,ArrayList<Integer> l)
     {
         if(i== arr.length || curr>k)
             return;
         if(curr == k)
         {
-            
-            Collections.sort(l);
-            if(!mp.containsKey(l))
-                mp.put(l,1);
+            list.add(l);
             return;
         }
-        call(mp,arr,k,i+1,curr,new ArrayList<Integer>(l));
+        call(list,arr,k,i+1,curr,new ArrayList<Integer>(l));
         l.add(arr[i]);
-         call(mp,arr,k,i,curr+arr[i],new ArrayList<Integer>(l));
-         call(mp,arr,k,i+1,curr+arr[i],new ArrayList<Integer>(l));
+         call(list,arr,k,i,curr+arr[i],new ArrayList<Integer>(l));
+         
     }
 }
