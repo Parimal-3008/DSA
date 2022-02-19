900523f2d5775f0cebda2d57437febce74df30be
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-02-19 09:30:00 +0530

    Time: 45 ms (5.05%), Space: 51.4 MB (5.05%) - LeetHub

---
+++ 980-unique-paths-iii/980-unique-paths-iii.java
@@ -0,0 +1,45 @@
+class Solution {
+    int count=0;
+    public int uniquePathsIII(int[][] arr) {
+        int s1=0,s2=0,e1=0,e2=0;int m=arr.length;
+        int n=arr[0].length;int tot = (m*n);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i][j]==1)
+                    {s1=i;e1=j;}
+                else if(arr[i][j]==2)
+                {s2=i;e2=j;}
+                else if(arr[i][j]==-1)
+                    tot--;
+            }
+        }
+        System.out.println(s1+" "+e1+" "+s2+" "+e2+" "+tot);
+        boolean[][] vis= new boolean[m][n];
+        Map<String ,Integer> mp = new HashMap<>();
+        call(arr,s1,e1,s2,e2,vis,tot,"");
+        return count;
+    }
+    public void call(int[][] arr, int s1,int e1,int s2,int e2,boolean[][] vis,int req,String str)
+    {
+        if(s1>=arr.length||s1<0||e1>=arr[0].length||e1<0||vis[s1][e1])
+            return;
+        if(s1==s2 && e1==e2 &&req==1 )
+        {
+           
+            count++;
+            return;
+        }
+        
+        if(arr[s1][e1]==-1)
+            return;
+        vis[s1][e1]=true;
+        call(arr,s1+1,e1,s2,e2,vis,req-1,str+"D");
+        call(arr,s1,e1+1,s2,e2,vis,req-1,str+"R");
+        call(arr,s1-1,e1,s2,e2,vis,req-1,str+"U");
+        call(arr,s1,e1-1,s2,e2,vis,req-1,str+"L");
+        vis[s1][e1]=false;
+        
+    }
+}
