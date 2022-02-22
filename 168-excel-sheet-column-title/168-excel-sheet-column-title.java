c45f3d22bf5c307e42acd89c932ffcdd1bf9c799
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-02-22 12:17:19 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (29.76%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -0,0 +1,13 @@
+class Solution {
+    // 26 26+676 18278
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while(n > 0){
+            n--;
+            int curr = n%26;
+            n /= 26;
+            sb.append((char)(curr+'A'));
+        }
+        return sb.reverse().toString();
+    }
+}
