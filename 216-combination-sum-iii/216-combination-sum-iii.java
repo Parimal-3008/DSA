296f03c18b2c4247095f7d2dc81ec843b6d521a6
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-02-23 17:41:07 +0530

    Time: 4 ms (6.01%), Space: 43.1 MB (5.54%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> list = new LinkedList<>();
+        call(1,k,n,new LinkedList<>(),list);
+        return list;
+    }
+    public void call(int i,int k,int n, List<Integer> temp, List<List<Integer>> list  )
+    {
+        if(k==0&&n==0)
+        {
+            list.add(temp);
+            return;
+
+        }
+        if(k==10||n<0)
+            return;
+        for(int j=i;j<=9;j++)
+        {
+            temp.add(j);
+            call(j+1,k-1,n-j,new LinkedList<>(temp),list);
+            temp.remove(temp.size()-1);
+        }
+        
+    }
+}
