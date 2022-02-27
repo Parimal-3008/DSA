fe7390767ad8a8c8f4637e416dbb2f750c53bf4b
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-02-27 21:21:33 +0530

    Time: 5 ms (16.17%), Space: 42.4 MB (37.48%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -14,41 +14,42 @@
  * }
  */
 class Solution {
+   
     class Edge
     {
-        long min;
-        long max;
+        int min;
+        int max;
         public Edge()
         {
-            this.min=Long.MAX_VALUE;
-            this.max=Long.MIN_VALUE;
+            this.min=Integer.MAX_VALUE;
+            this.max=Integer.MIN_VALUE;
         }
     }
     public int widthOfBinaryTree(TreeNode root) {
-       
-            
-            
-        Edge arr[] = new Edge[4000];
+       Edge arr[] = new Edge[4000];
+         int ind[]= new int[4000];
+        Arrays.fill(ind,Integer.MAX_VALUE);
         for(int i=0;i<4000;i++)
             arr[i]=new Edge();
-        dfs(root,1,0,arr);
-        long m= 1;
+        dfs(root,1,0,arr,ind);
+        int m= 1;
         for(int i=1;i<4000;i++)
            {
-            //System.out.println(arr[i].max+" "+arr[i].min);
-            if(arr[i].max!=Long.MIN_VALUE&&arr[i].min!=Long.MAX_VALUE)
+            if(arr[i].max!=Integer.MIN_VALUE&&arr[i].min!=Integer.MAX_VALUE)
             m=Math.max(m,arr[i].max-arr[i].min+1);
-        }
-       return (int)m;
+          }
+       return m;
     }
-    public void dfs(TreeNode root, long x,long y,Edge arr[])
+    public void dfs(TreeNode root, int x,int y,Edge arr[],int[] ind)
     {
         if(root==null)
             return;
-        arr[(int)y].min=Math.min(x, arr[(int)y].min);
-        arr[(int)y].max=Math.max(x,arr[(int)y].max);
-        dfs(root.left,2*x,y+1,arr);
-        dfs(root.right,2*x+1,y+1,arr);
+        if(ind[y]==Integer.MAX_VALUE)
+            ind[y]=x;
+        arr[y].min=Math.min(x-ind[y], arr[y].min);
+        arr[y].max=Math.max(x-ind[y],arr[y].max);
+        dfs(root.left,2*x,y+1,arr,ind);
+        dfs(root.right,2*x+1,y+1,arr,ind);
         
     }
 }
