a2426895d41f8edf947a1c03762976d84edf886a
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-03-01 13:47:51 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (6.94%) - LeetHub

---
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     int max=-1;
+    TreeNode yo = new TreeNode();       
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+      call(root,0);
+        return yo;
+    }
+    public int call(TreeNode root,int d)
+    {
+        if(root==null)
+            return d-1;
+        if(d>max)
+        {
+           max=d;
+        }
+        int p= call(root.left,d+1);
+        int q= call(root.right,d+1);
+        if(p==max && q==max)
+            yo=root;
+        return Math.max(p,q);
+        
+    }
+}
