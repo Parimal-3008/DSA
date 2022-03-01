88cd7f3a9ed1955fd639b6bd1fc3bffe01605cdd
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-03-01 20:50:43 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (38.47%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,8 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
         int dp[]= new int[n+1];
-        Arrays.fill(dp,-1);
-        return call(dp,n);
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            if(i==2){
+                dp[i]=2;continue;}
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+        // Arrays.fill(dp,-1);
+        // return call(dp,n);
     }
     public int call(int []dp, int n)
     {
