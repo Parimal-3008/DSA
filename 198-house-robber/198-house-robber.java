4d16b4372d95eb73864ac09c5007533b949ed7af
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-03-02 20:06:17 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (28.64%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -2,16 +2,20 @@ class Solution {
     public int rob(int[] arr) {
         int[] dp= new int[arr.length+1];
         Arrays.fill(dp,-1);
-        return call(arr,dp,arr.length-1);
+        // for(int i=n-1;i>0;i--)
+        // {
+        //     int p = arr[i]+dp[n-2]
+        // }
+        return call(arr,dp,0);
     }
     public int call(int[] arr, int[] dp, int n)
     {
-        if(n<0)
+        if(n>=arr.length)
             return 0;
         if(dp[n]!=-1)
             return dp[n];
-        int p = arr[n]+call(arr,dp,n-2);
-        int q = call(arr,dp,n-1);
+        int p = arr[n]+call(arr,dp,n+2);
+        int q = call(arr,dp,n+1);
         return dp[n]=Math.max(p,q);
     }
 }
