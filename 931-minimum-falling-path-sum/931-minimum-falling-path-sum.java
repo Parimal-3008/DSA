95fbf98ab05cec4a15d0f1f6608188d5d751a983
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-03-13 13:28:37 +0530

    Time: 2 ms (93.22%), Space: 43 MB (81.76%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,59 +1,31 @@
 class Solution {
     public int minFallingPathSum(int[][] arr) {
-        int n= arr.length;
+        int n =arr.length;
         int dp[][] = new int[n][n];
-         for(int i=0;i<n;i++)
-             for(int j=0;j<n;j++)
-                 dp[i][j]=Integer.MAX_VALUE;
-       
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0)
-                {dp[i][j]=arr[i][j];continue;}
-                 int a=arr[i][j];int c=arr[i][j];int b=arr[i][j];
-                if(i-1>=0)
-                    a+=dp[i-1][j];
-                else
-                    a+=100000000;
-                if(i-1>=0&&j-1>=0)
-                    b+=dp[i-1][j-1];
-                else
-                    b+=100000000;
-                if(i-1>=0&&j+1<n)
-                    c+=dp[i-1][j+1];
-                else
-                    c+=100000000;
-               dp[i][j]=Math.min(a,Math.min(b,c));
-            }
+           for(int j=0;j<n;j++)
+               dp[i][j]=-1;
         }
-        int min=Integer.MAX_VALUE;
-        
+        int min = Integer.MAX_VALUE;
         for(int i=0;i<n;i++)
-            min=Math.min(min,call(arr,dp,n-1,i));
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //         System.out.print(dp[i][j]+" ");
-        //     System.out.println();
-        // }
+            min = Math.min(min,call(arr,dp,0,i));
         return min;
+            
     }
     public int call(int[][] arr, int[][] dp, int i, int j)
     {
-        if(i<0||j<0||j==arr.length)
-            return 100000000;
-        if(i==0)
+        int n= arr.length;
+        if(j<0||j==n)
+            return 10000000;
+        if(i==n-1)
             return arr[i][j];
         if(dp[i][j]!=-1)
             return dp[i][j];
-        int a=arr[i][j];int c=arr[i][j];
-        int b=arr[i][j];
-        
-            a+=call(arr,dp,i-1,j-1);
-            b+=call(arr,dp,i-1,j);
-            c+=call(arr,dp,i-1,j+1);
-        return dp[i][j]=Math.min(a,Math.min(b,c));
+        int a =arr[i][j]+call(arr,dp,i+1,j);
+        int b =arr[i][j]+call(arr,dp,i+1,j+1);
+        int c=arr[i][j]+call(arr,dp,i+1,j-1);
+        //System.out.println(Math.min(a,Math.min(b,c)));
+        return dp[i][j]= Math.min(a,Math.min(b,c));
     }
 }
