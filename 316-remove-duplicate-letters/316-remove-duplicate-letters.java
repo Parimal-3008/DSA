8b5be000d9f59a16a68bea5f81d0cd80359ce84d
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-03-18 10:28:05 +0530

    Time: 4 ms (75.34%), Space: 42.9 MB (46.20%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -1,5 +1,12 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
+        /*
+        we store thr freq of each char present in the string
+        we check if we get an elemnt smaller than previously used elemnt
+        then we have a chance of popping it as it occurs in next part hence freq array will be used
+        and if we pop the element then we didnt use it so we sey it in vis as false and if it didnt             occur in next part we are bound to use it and we cant skip it.
+        
+        */
         int[] freq=new int[26];
         for(int i=0;i<s.length();i++)
         {
