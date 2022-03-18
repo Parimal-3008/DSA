ea2dd8795d0b9b836748329794ecc1956703c4c2
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-03-18 16:02:34 +0530

    Time: 12 ms (45.15%), Space: 51 MB (26.42%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -2,21 +2,31 @@ class Solution {
     public int change(int k, int[] arr) {
         int n = arr.length;
         int[][] dp = new int[n][k+1];
-        for(int i=0;i<n;i++)
-            for(int j=0;j<=k;j++)
-                dp[i][j]=-1;
+        // for(int i=0;i<n;i++)
+        //     for(int j=0;j<=k;j++)
+        //         dp[i][j]=-1;
         if(k==0)
             return 1;
-        int p =call(arr,dp,k,n-1);
-         // for(int i=0;i<n;i++)
-         //   { 
-         //     for(int j=0;j<=k;j++)
-         //        System.out.print(dp[i][j]+" ");
-         //     System.out.println();
-         //    }
-
-
-        return p;
+        for(int i=0;i<n;i++)
+            dp[i][0]=1;
+        for(int i=0;i<=k;i++)
+           { 
+            if(i%arr[0]==0)
+                dp[0][i]=1;
+           }
+        for(int i=01;i<n;i++)
+        {
+            for(int j=1;j<=k;j++)
+            {
+                 int not= dp[i-1][j];
+                 int pic=0;
+                 if(arr[i]<=j)
+                    pic=dp[i][j-arr[i]];
+                 dp[i][j]=pic+not;
+            }
+        }
+        return dp[n-1][k];
+      // return call(arr,dp,k,n-1);;
     }
     public int call(int[] arr, int [][] dp, int k, int i)
     {
