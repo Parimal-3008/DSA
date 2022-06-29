341cbf4541870b5199a4893604f465b0eb6fbbc7
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-06-29 23:20:03 +0530

    Time: 2 ms (18.57%), Space: 43 MB (46.15%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,45 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        double res=1.0;
-        
-        if(n==-2147483648)
-                {
-        n/=2;
-            x*=x;
-        }
-        if(n>0)
-        {
-             while(n>0)
-           {
-            if(n%2!=0)
-            {
-                res*=(double)x;
-            }
-            x=x*x;
-            n/=2;
-           }
-        return res;
-
-        }
-        else if(n<0)
-        {
-            n*=(-1);
-            
-             while(n>0)
-           {
-            if(n%2!=0)
-            {
-                res/=(double)x;
-            }
-            x=x*x;
-            n/=2;
-           }
-            if(res==1)
-                res/=x;
-        return res;
-        }
-       
-            return 1.0;
-       
+      // boolean status= false;
+      //   if(n>0)
+      //     status = true;
+      // n= Math.abs(n);
+      if(n==0)
+        return 1;
+      if(n==1)
+      return x;
+    if(n==-1)
+      return 1.0/x;
+    double ans = myPow(x,n/2);
+    if((n-n/2)==n/2)
+      return ans*ans;
+     if(n<0)
+       return ans*ans/x;
+    return ans*ans*x;
     }
+//   public double call(double x, int n)
+//   {
+    
+    
+//   }
 }
