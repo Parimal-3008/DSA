40197d1c789f489b9b485dc52c535a34cabc8142
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-08-19 16:47:44 +0530

    Time: 1 ms (11.70%), Space: 41.2 MB (26.10%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.java
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.java
@@ -1,26 +1,15 @@
 class Solution {
     public int kthGrammar(int n, int k) {
-       int p=call(n,k);
-       return  p;
-    }
-    public int call(int n,int k )
-    {
         if(k==1)
-           return 0;
-        if(k==2)
-            return 1;
-        int q=0;
-        if(k>(int)Math.pow(2,n-2))
-        {
-            int k1=-(int)Math.pow(2,n-2) + k;
-            q = call(n-1,k1);
-        }
-        else
-            { q = call (n-1,k);return q;}
-        
-        if(q==1)
-            return 0;
+          return 0;
+      if(k==2)
         return 1;
+      int p = kthGrammar(n,(int)Math.ceil((double)k/2.0));
+      if(k%2==0)
+    {  if(p==1)
+        return 0;
+      else
+        return 1;}
+      return p;
     }
-   
 }
