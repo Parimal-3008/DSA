e5afaa47a7a51dc257877c2ee4209807f6983ba9
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-08-19 12:47:05 +0530

    Time: 114 ms (8.64%), Space: 79.1 MB (5.06%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,36 +1,12 @@
 class Solution {
-    class Edge
-    {
-        int x;
-        public Edge(int x)
-            {this.x=x;}
-    }
-    class EdgeComparator implements Comparator<Edge>
-    {
-        @Override
-        public int compare(Edge s1, Edge s2)
-        {
-            if(s1.x>=s2.x)
-                return 1;
-            else
-                return -1;
-        }
-    }
     public int findKthLargest(int[] arr, int k) {
-        PriorityQueue<Edge> pq = new PriorityQueue<Edge> (k, new EdgeComparator());
-        for(int i=0;i<arr.length;i++)
-        {
-            
-            pq.offer(new Edge(arr[i]));
-            if(pq.size()>k)
-                pq.poll();
-        }
-        System.out.println(pq);
-        int a=-1;
-        // while(!pq.isEmpty()&&k>0)
-        // {
-        //     a = pq.poll().x;k--;
-        // }
-        return pq.peek().x;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> Integer.compare(a,b));
+      for(int i:arr)
+      {
+        pq.offer(i);
+        if(pq.size()>k)
+          pq.poll();
+      }
+      return pq.peek();
     }
 }
