e9c4a0397c00d9c117eddf5cf4763943cdf8c1e7
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-08-20 23:34:11 +0530

    Time: 1 ms (86.54%), Space: 40.5 MB (84.60%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -1,27 +1,19 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> list = new LinkedList<>();
-        call(1,k,n,new LinkedList<>(),list);
-        return list;
+        List<List<Integer>> list = new ArrayList<>();
+        call(1,k,n,list,new ArrayList<>());
+      return list;
     }
-    public void call(int i,int k,int n, List<Integer> temp, List<List<Integer>> list  )
+  public void call(int i, int k, int n, List<List<Integer>> list , List<Integer> l)
+  {
+    if(k==0 && n==0)
     {
-        if(k==0&&n==0)
-        {
-            list.add(temp);
-            return;
-
-        }
-        if(k==10||n<0)
-            return;
-        for(int j=i;j<=9;j++)
-        {
-            if(n<j)
-                return;
-            temp.add(j);
-            call(j+1,k-1,n-j,new LinkedList<>(temp),list);
-            temp.remove(temp.size()-1);
-        }
-        
+      list.add(l);return;
     }
+    if(k<0||n<0||i>9)
+      return;
+    call(i+1,k,n,list,new ArrayList<>(l));
+    l.add(i);
+    call(i+1,k-1,n-i,list,new ArrayList<>(l));
+  }
 }
