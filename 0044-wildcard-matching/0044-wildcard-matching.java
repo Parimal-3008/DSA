3ae0181e7ce54461c8f5591bf3fbed322072c99b
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2022-12-08 07:39:55 +0530

    Time: 40 ms (63.73%), Space: 54.7 MB (9.35%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,29 +2,12 @@ class Solution {
     public boolean isMatch(String s, String p) {
         char arr1[]= s.toCharArray();
       char arr2[]= p.toCharArray();
-      char prev='.';int count =0;
-      for(int i=0;i<arr2.length;i++)
-      {
-        if(arr2[i]==prev && prev=='*')
-        {arr2[i]='.';count++;}
-        else
-          prev=arr2[i];
-      }
-      char arr3[]= new char[arr2.length-count];
-      int j=0;
-      for(int i=0;i<arr2.length;i++)
-      {
-        if(arr2[i]!='.')
-          arr3[j++]=arr2[i];
-      }
-      for(char i:arr3)
-        System.out.print(i);
     
       int dp[][]= new int[arr1.length][arr2.length];
       for(int i[]:dp)
         Arrays.fill(i,-1);
       
-      int p2 = call(arr1,arr3,0,0,dp);
+      int p2 = call(arr1,arr2,0,0,dp);
       return p2!=0;
     }
   public int call(char[] arr1, char[] arr2,int i, int j, int[][] dp)
@@ -44,16 +27,15 @@ class Solution {
       return 0;
     if(dp[i][j]!=-1)
     return dp[i][j];
-    
     if(arr1[i]==arr2[j] || arr2[j]=='?')
-       return dp[i][j]= call(arr1,arr2,i+1,j+1,dp);
+     return dp[i][j]= call(arr1,arr2,i+1,j+1,dp);
     else if(arr2[j]=='*')
-    {      
+    {
+      
        int a = call(arr1,arr2,i+1,j,dp);
        int b = call(arr1,arr2,i+1,j+1,dp);
-       int c = call(arr1,arr2,i,j+1,dp);
-      // System.out.println(a+b+c);
-       return dp[i][j]=a+b+c;
+      int c = call(arr1,arr2,i,j+1,dp);
+      return dp[i][j]=a+b+c;
     }
     else
       return dp[i][j]= 0;
