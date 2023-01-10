889260917a31ec98a836c334320c3427554ac2ab
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2023-01-10 14:25:33 +0530

    Time: 69 ms (16.77%), Space: 42.1 MB (11.18%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
@@ -1,33 +1,18 @@
 class Solution {
-    public int peopleAwareOfSecret(int n, int d, int f) {
-        long arr[] = new long[1005];
-        
-        arr[1]=1;//arr[1+d]=2;
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        long arr[]= new long[10000];
+      long mod = 1000000007;
+      arr[1]=1;
+      long p =0;
       for(int i=1;i<=n;i++)
       {
-        int k=i+d;
-          for(int j=f-d;j>0;j--)
-          {
-            if(k>n+1)
-              break;
-              arr[k]=(arr[k]+arr[i])%1000000007;
-               k++;
-            
-          }
-        // if(i-d-1>=0)
-        //   arr[i-d-1]--;
+        if(arr[i]==0)
+          continue;
+        for(int j=i+delay;j<i+forget&& j<arr.length;j++)
+          arr[j]= (arr[j]+arr[i])%mod;
       }
-      long ans=0;
-      for(int i=n;i>=n+1-f;i--)
-      {
-        //System.out.print(arr[i]+",");
-        if(arr[i]>=0)
-          ans=(ans+arr[i])%1000000007;
-      }
-      long res=0;
-      // for(int i=n;i>=0;i--)
-      //   System.out.print(((res+=arr[i])%1000000007)+",");
-     // System.out.println(ans);
-        return (int)ans%1000000007;
+      for(int i=n-forget+1;i<=n;i++)
+        p= (p+ arr[i])%mod;
+      return (int)p;
     }
 }
