f2931600e0291f978e8fdab65881cf99cd11deec
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2023-01-25 07:55:03 +0530

    Time: 170 ms (18.56%), Space: 66 MB (54.49%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int closestMeetingNode(int[] arr, int node1, int node2) {
+        Map<Integer,Integer> mp = new HashMap<>();
+      int curr =0;
+      while(node1!=-1)
+      {
+        if(mp.containsKey(node1))
+          break;
+        mp.put(node1,curr);
+        curr++;
+        node1=arr[node1];
+      }
+      int min = Integer.MAX_VALUE;
+      int ind = -1;
+      curr=0;
+      Map<Integer,Integer> mp2 = new HashMap<>();
+       while(node2!=-1)
+      {
+         if(mp2.containsKey(node2))
+           break;
+       if(mp.containsKey(node2))
+       {
+         if(min>Math.max(mp.get(node2),curr))
+         {
+           min = Math.max(mp.get(node2),curr);
+           ind = node2;
+         }
+         else if(min ==Math.max(mp.get(node2),curr))
+         {
+           ind = Math.min(ind,node2);
+         }
+       }
+         
+         mp2.put(node2,curr);
+        curr++;
+        node2=arr[node2];
+      }
+      System.out.println(mp);
+      System.out.println(mp2);
+      
+      return ind;
+    }
+}
