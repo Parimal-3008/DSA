b50ff6667520a9e149ad82add3a753c76dc36b12
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2023-03-15 13:59:54 +0530

    Time: 589 ms (5.01%), Space: 51.1 MB (36.10%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+        int count =0;
+      for(int i:arr)
+      {
+        if(i==1)
+          count++;
+      }
+      if(count ==0)
+        return 1;
+      for(int i=0;i<arr.length;i++)
+      {
+        if(arr[i]>arr.length|| arr[i]<1)
+          arr[i]=1;        
+      }
+      for(int i=0;i<arr.length;i++)
+      {
+        int p = arr[i];
+        if(p<0)
+          p*=(-1);
+        p--;
+        if(arr[p]>0)
+          arr[p]*=(-1);
+      }
+      for(int i:arr)
+        System.out.print(i+" ");
+      System.out.println();
+      for(int i=0;i<arr.length;i++)
+      {
+        if(i!=0 && arr[i]>0)
+          return i+1;
+      }
+      return arr.length+1;
+     
+     
+    }
+}
