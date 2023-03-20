6551e699ca12b185f88056abfc4c14b8ef1f3f67
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2023-03-21 00:03:38 +0530

    Time: 22 ms (44.57%), Space: 44.8 MB (46.74%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -0,0 +1,47 @@
+class Solution {
+  int mod = 1000000007;
+    public int numWays(int k, int n) {
+      int dp[][]= new int[501][501];
+      for(int i[]:dp)
+        Arrays.fill(i,-1);
+      return call(0,k,n,dp);  
+    }
+  public int call(int i, int k, int n,int[][] dp)
+  {
+    if(k==0 && i==0)
+      return 1;
+    if(i==n)
+      return 0;    
+    if(k<0)
+      return 0;
+    int ans =0;
+   
+      int a = 0;
+      int b = 0;
+      int c =0;
+    int j=0;
+    if(dp[i][k]!=-1)
+      return dp[i][k];
+      if(i==0)
+      {
+        a = call(i+1,k-j-1,n,dp);
+        b = call(i,k-j-1,n,dp);
+      }
+      else if(i==n-1)
+      {
+        a = call(i-1,k-j-1,n,dp);
+        b = call(i,k-j-1,n,dp);
+      }
+      else
+      {
+        a = call(i-1,k-j-1,n,dp);
+        b = call(i,k-j-1,n,dp);
+        c = call(i+1,k-j-1,n,dp);
+      }
+      ans += (((a+b)%mod)+c)%mod;
+      while(ans<0)
+        ans+=mod;
+    
+    return dp[i][k]=ans;
+  }
+}
