9fd597b42b5890682857afe6722d81f20a827d96
Parimal-3008 <69786459+Parimal-3008@users.noreply.github.com>
2023-04-02 11:43:06 +0530

    Time: 3 ms (89.17%), Space: 43 MB (37.56%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minTaps(int n, int[] ranges) {
+          int[] arr = new int[n + 1];
+        for(int i = 0; i < ranges.length; i++) {
+            if(ranges[i] == 0) continue;
+            int left = Math.max(0, i - ranges[i]);
+            arr[left] = Math.max(arr[left], i + ranges[i]);
+        }
+        int end = 0, farCanReach = 0, cnt = 0;        
+        for(int i = 0; i < arr.length && end < n; end = farCanReach) {
+            cnt++;
+            while(i < arr.length && i <= end) {
+                farCanReach = Math.max(farCanReach, arr[i++]);                            
+            }
+            if(end == farCanReach) return -1; 
+        }
+        return cnt;
+    }
+}
