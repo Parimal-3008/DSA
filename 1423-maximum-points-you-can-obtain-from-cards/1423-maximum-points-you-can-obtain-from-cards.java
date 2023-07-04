fbf7cb42937d1b849b1b8e1d031e5cf6e77386ae
Parimal Yeole <69786459+Parimal-3008@users.noreply.github.com>
2023-07-04 12:19:03 +0530

    Time: 2 ms (94.68%), Space: 56.1 MB (8.09%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,23 +1,20 @@
 class Solution {
     public int maxScore(int[] arr, int k) {
-        int max=0;
-      int curr=0;
-      for(int i=0;i<k;i++)
-        max+=arr[i];
-      
-      for(int i =arr.length-k;i<arr.length;i++)
-      {
-        curr+=arr[i];
-        max=Math.max(max,curr);
-         //System.out.println(curr);
-      }
-      for(int i=arr.length-k+1;i<arr.length;i++)
-      {
-          curr-=arr[i-1];
-        curr+=arr[(i+k-1)%arr.length];
-       // System.out.println(curr);
-        max=Math.max(max,curr);
-      }
-      return max;
+        int tot = 0;
+        for(int i:arr)
+            tot+=i;
+         k= arr.length-k;
+        int ans = 0;
+        int curr = 0;
+        for(int i=0;i<k;i++)
+            curr+=arr[i];
+        ans = tot-curr;
+        for(int i=k;i<arr.length;i++)
+        {
+            curr-=arr[i-k];
+            curr+=arr[i];
+            ans = Math.max(ans,tot-curr);
+        }
+        return ans;
     }
 }
