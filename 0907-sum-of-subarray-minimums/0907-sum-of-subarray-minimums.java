041a2efef0d3e13415bdbbed3fbb8bb2927e5d41
Parimal Yeole <69786459+Parimal-3008@users.noreply.github.com>
2023-07-15 22:32:50 +0530

    Time: 148 ms (29.80%), Space: 45.4 MB (89.51%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,80 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+       int[] right = new int[arr.length];
+        int[] left = new int[arr.length];
+        // Arrays.fill(right,arr.length);
+        // Arrays.fill(left,-1);
+        call1(arr,right);
+        call2(arr,left);
+        
+        long ans = 0;
+        long mod = 1000000007;
+        // for(int i:left)
+        //     System.out.print(i+" ");
+        // System.out.println();
+        // for(int i:right)
+        //     System.out.print(i+" ");
+        for(int i=0;i<arr.length;i++)
+        {
+            long temp =  right[i]*left[i];
+            temp= (temp*(long)arr[i])%mod;
+            ans = (ans+temp)%mod;
+            while(ans<0)
+                ans+=mod;
+        }
+        return (int)ans;        
+    }
+    public void call1(int[] arr, int[] dp)
+    {
+        Stack<Integer> st = new Stack<>();
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            dp[i]=arr.length;
+            while(!st.isEmpty())
+            {
+               if(arr[st.peek()]<=arr[i])
+               {
+                   dp[i]=st.peek();
+                   break;
+               }
+                else
+                    st.pop();
+            }
+            dp[i]-=i;
+            st.push(i);
+            
+        }
+    }
+    public void call2(int[] arr, int[] dp)
+    {
+         Stack<Integer> st = new Stack<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            dp[i]=-1;
+            while(!st.isEmpty())
+            {
+               if(arr[st.peek()]<arr[i])
+               {
+                   dp[i]=st.peek();
+                   break;
+               }
+                else
+                    st.pop();
+            }
+            dp[i]=i-dp[i];
+            st.push(i);
+            
+        }
+    }
+}
+/*
+Right: 1 
+*/
+// right[]: 1 4 4 4 ->  0 3 3 3
+// left[]: -1 -1 1 2 -> 0 0 2 3
+    // 3+4+
+
+// 4 3 3 4 5
+// -1 0 1 0 -1
+// right: 3 2 2 3 4
+// left:  0 1 2 1 0
