8af1216a965cde4a5c2818f171250cb8633b10c6
Parimal Yeole <69786459+Parimal-3008@users.noreply.github.com>
2023-07-20 11:06:25 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (19.06%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return call(root,false);
+    }
+    public int call(TreeNode root, boolean st)
+    {
+        if(root == null)
+            return 0;
+        if(root.left == null && root.right == null)
+            return st?root.val:0;
+        
+        return call(root.left,true)+ call(root.right,false);
+    }
+}
