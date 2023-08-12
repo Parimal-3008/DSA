3bb27ac1787ebec95ccaac479b6d92d8b2801fec
Parimal Yeole <69786459+Parimal-3008@users.noreply.github.com>
2023-08-12 12:27:20 +0530

    Time: 64 ms (5.69%), Space: 44 MB (95.54%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,28 +1,27 @@
 class Solution {
     public int minInsertions(String str) {
-        String rev ="";
-      for(int i=str.length()-1;i>=0;i--)
-        rev = rev + String.valueOf(str.charAt(i));
-      int dp[][]= new int[str.length()][rev.length()];
-     // System.out.println(str+" "+rev);
-      for(int i[]:dp)
-        Arrays.fill(i,-1);
-      int ans = call(str,rev,0,0,dp);
-    //  System.out.println(ans);
-      return str.length()-ans;
+        String str2= "";
+        for(int i=str.length()-1;i>=0;i--)
+            str2=str2+String.valueOf(str.charAt(i));
+        int[][] dp = new int[str.length()][str.length()];
+        for(int i[]:dp)
+            Arrays.fill(i,-1);
+        int a = call(str,str2,0,0,dp);
+        return str.length()-a;
     }
-  public int call(String str1, String str2, int i, int j,int[][] dp)
-  {
-    if(i==str1.length()||j==str2.length())
-      return 0;
-    if(dp[i][j]!=-1)
-      return dp[i][j];
-    char ch1 = str1.charAt(i);
-    char ch2 = str2.charAt(j);
-    if(ch1 == ch2)
-      return dp[i][j]= 1 + call(str1,str2,i+1,j+1,dp);
-    int a = call(str1,str2,i+1,j,dp);
-    int b = call(str1,str2,i,j+1,dp);
-    return dp[i][j]=Math.max(a,b);
-  }
+    public int call(String str1,String str2,int i, int j, int[][] dp)
+    {
+        if(i==str1.length() || j== str2.length())
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int a= 0;int b = 0;int c = 0;
+        if(str1.charAt(i)==str2.charAt(j))
+        {
+            a = 1 + call(str1,str2,i+1,j+1,dp);            
+        }
+        b = call(str1,str2,i+1,j,dp);
+        c = call(str1,str2,i,j+1,dp);
+        return dp[i][j]=Math.max(a,Math.max(b,c));
+    }
 }
