1ac6dc2e449ba64a71c6bfbad8b4ab16e2846dca
Parimal Yeole <69786459+Parimal-3008@users.noreply.github.com>
2023-08-21 11:44:23 +0530

    Time: 65 ms (9.68%), Space: 54.7 MB (5.64%) - LeetHub

---
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.java
@@ -0,0 +1,40 @@
+class Solution {
+    int[][][] dp = new int[101][101][101];
+    public int palindromePartition(String str, int k) {
+        for(int i[][]:dp)
+            for(int j[]:i)
+            Arrays.fill(j,-1);
+        return call(str,0,0,k);
+    }
+    public int call(String str,int i, int start, int k)
+    {
+        
+        if(i>=str.length())
+            return Integer.MAX_VALUE-10000;
+        if(k==1)
+        {
+            int p = change(str,i,str.length()-1);
+            return p;
+        }
+        if(dp[i][start][k]!=-1)
+            return dp[i][start][k];
+        int a = call(str,i+1,start,k);
+        int b = change(str,start,i)+ call(str,i+1,i+1,k-1);
+        return dp[i][start][k]= Math.min(a,b);
+    }
+    public int change(String str, int i, int j)
+    {
+        int ans = 0;
+         // System.out.print(i+" "+j+" ");
+       
+        while(i<=j)
+        {
+            if(str.charAt(i)!=str.charAt(j))
+                ans++;
+            i++;j--;
+        }
+        // System.out.print(ans);
+        // System.out.println();
+        return ans;
+    }
+}
