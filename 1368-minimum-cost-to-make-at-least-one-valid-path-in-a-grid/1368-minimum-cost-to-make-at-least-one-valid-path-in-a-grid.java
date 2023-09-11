bfa72ed71172c3c090de7fdadc2716b7398dedc0
Parimal Yeole <69786459+Parimal-3008@users.noreply.github.com>
2023-09-11 09:56:21 +0530

    Time: 35 ms (35.35%), Space: 43.8 MB (66.67%) - LeetHub

---
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int minCost(int[][] arr) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[2],b[2]));
+        pq.add(new int[]{0,0,0});
+        int[][] dp = new int[arr.length][arr[0].length];
+        for(int i[]:dp)
+            Arrays.fill(i,Integer.MAX_VALUE);
+        dp[0][0]=0;
+        int m = arr.length; int n = arr[0].length;
+        while(!pq.isEmpty())
+        {
+            int temp[]= pq.poll();
+            int i = temp[0];
+            int j = temp[1];
+            int k = temp[2];
+            // if(i==m-1 && j==n-1)
+            if(i+1<m)
+            {
+                if(arr[i][j]==3 && dp[i+1][j]>dp[i][j])
+                {
+                    dp[i+1][j]=dp[i][j];
+                    pq.add(new int[]{i+1,j,dp[i+1][j]});
+                }
+                else if(dp[i+1][j]>dp[i][j]+1)
+                {
+                    dp[i+1][j]=dp[i][j]+1;
+                    pq.add(new int[]{i+1,j,dp[i+1][j]});
+                }
+            }
+            if(i-1>=0)
+            {
+                if(arr[i][j]==4 && dp[i-1][j]>dp[i][j])
+                {
+                    dp[i-1][j]=dp[i][j];
+                    pq.add(new int[]{i-1,j,dp[i-1][j]});
+                }
+                else if(dp[i-1][j]>dp[i][j]+1)
+                {
+                    dp[i-1][j]=dp[i][j]+1;
+                    pq.add(new int[]{i-1,j,dp[i-1][j]});
+                }
+            }
+            if(j+1<n)
+            {
+                if(arr[i][j]==1 && dp[i][j+1]>dp[i][j])
+                {
+                    dp[i][j+1]=dp[i][j];
+                    pq.add(new int[]{i,j+1,dp[i][j+1]});
+                }
+                else if(dp[i][j+1]>dp[i][j]+1)
+                {
+                    dp[i][j+1]=dp[i][j]+1;
+                    pq.add(new int[]{i,j+1,dp[i][j+1]});
+                }
+            }
+            if(j-1>=0)
+            {
+                if(arr[i][j]==2 && dp[i][j-1]>dp[i][j])
+                {
+                    dp[i][j-1]=dp[i][j];
+                    pq.add(new int[]{i,j-1,dp[i][j-1]});
+                }
+                else if(dp[i][j-1]>dp[i][j]+1)
+                {
+                    dp[i][j-1]=dp[i][j]+1;
+                    pq.add(new int[]{i,j-1,dp[i][j-1]});
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
